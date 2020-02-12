091414c2cee3534acb87045e4dd49c63f50d35e4
Chirag Senjaliya <chirag.senjaliya@softwareimc.com>
2020-02-12 18:42:17 +0530

    Roles and permission

--- app/Admin.php
+++ app/Admin.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class Admin extends Authenticatable
 {
